6670629c99ab904d40485a485c248248538c1473
Simone Busoli <simone.busoli@gmail.com>
2021-02-28 11:44:54 +0100

    chore: formatting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const startServer = require('./lib/server')
 const getConfig = require('./lib/config')
 
 const main = async () => {
-  process.on('unhandledRejection', err => {
+  process.on('unhandledRejection', (err) => {
     console.error(err)
     process.exit(1)
   })
@@ -16,7 +16,7 @@ const main = async () => {
 
   for (const signal of ['SIGINT', 'SIGTERM']) {
     process.on(signal, () =>
-      server.close().then(err => {
+      server.close().then((err) => {
         console.log(`close application on ${signal}`)
         process.exit(err ? 1 : 0)
       })
